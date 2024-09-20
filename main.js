3d74621155224a9b934e51f1668caef1a63450c8
MUHAMMAD SALEH ZUBAIR <140771238+HAX3R302@users.noreply.github.com>
2024-09-20 16:27:38 +1000

    Update main.js

--- main.js
+++ main.js
@@ -77,18 +77,18 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +916909137213")))
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +923253444558")))
             process.exit(0)
          }
       } else {
-         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Es teams said you should Please type your WhatsApp number 😍\nFor example: +2348187637779 : `)))
+         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`• Hαxєr-ʙᴜɢ • you should Please type your WhatsApp number 😍\nFor example: +923253444558: `)))
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          // Ask again when entering the wrong number
          if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
             console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +2348187637779")))
 
-            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Es teams said you should Please type your WhatsApp number 😍\nFor example: +2348187637779 : `)))
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`• Hαxєr-ʙᴜɢ • you should Please type your WhatsApp number 😍\nFor example: +923253444558: `)))
             phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
             rl.close()
          }
@@ -175,9 +175,9 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
 			await delay(1999)
             console.log(chalk.yellow(`\n\n                  ${chalk.bold.blue(`[ ${botname} ]`)}\n\n`))
             console.log(chalk.cyan(`< ================================================== >`))
-	        console.log(chalk.magenta(`\n${themeemoji} YT CHANNEL: Xeon`))
-            console.log(chalk.magenta(`${themeemoji} GITHUB: DGXeon `))
-            console.log(chalk.magenta(`${themeemoji} INSTAGRAM: @unicorn_xeon13 `))
+	        console.log(chalk.magenta(`\n${themeemoji} YT CHANNEL: MuhammadSalehZubair`))
+            console.log(chalk.magenta(`${themeemoji} TIKTOK: @itxsalaih `))
+            console.log(chalk.magenta(`${themeemoji} INSTAGRAM: @itxsalaih `))
             console.log(chalk.magenta(`${themeemoji} WA NUMBER: ${owner}`))
             console.log(chalk.magenta(`${themeemoji} CREDIT: ${wm}\n`))
         }
